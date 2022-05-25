d9f03f55337ff0f9ec68f4987d6ce3844c4b764c
Yahia-2010 <yahiaberash8@gmail.com>
2022-05-25 14:59:47 +0300

    adding the url location indication

--- frontend/src/components/Sidebar/SideButton.jsx
+++ frontend/src/components/Sidebar/SideButton.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const SideButton = ({ link, label, icon }) => {
+  const l = useLocation();
+  const location = l.pathname;
+  // indication
   return (
     <Link
       to={link}
-      className="flex justify-between items-center w-full mr-16 group"
+      className={`flex justify-between items-center w-full mr-16 group ${
+        location === `/${link.replace("/", "")}`
+          ? "font-semibold text-color-primary"
+          : "font-normal"
+      }`}
     >
-      {icon}
+      <span
+        className={
+          location === `/${link.replace("/", "")}`
+            ? "child:child:fill-color-primary"
+            : ""
+        }
+      >
+        {icon}
+      </span>
       <span className="transition group-hover:text-color-primary capitalize">
         {label}
       </span>
